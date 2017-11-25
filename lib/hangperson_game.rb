29c1e752d8849e41cb8544398bb3b77d08f18a88
Brent Vardy <brent.vardy@challengelogic.net>
2017-11-25 14:20:31 +0000

    completed: Part 1: Hangperson

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,5 +1,7 @@
 class HangpersonGame
 
+  attr_accessor :word, :guesses, :wrong_guesses
+
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
@@ -10,6 +12,51 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  def guess(guess)
+
+    if guess == nil || guess == '' || guess == '%'
+      raise ArgumentError, 'Invalid argument'
+    end
+
+    guess.downcase!
+    if @word.include? guess
+      if !@guesses.include? guess
+      @guesses << guess
+      else
+        false 
+      end
+    else
+      if !@wrong_guesses.include? guess
+        @wrong_guesses << guess
+        else
+          false 
+        end
+    end
+  end
+
+  def word_with_guesses
+    matches = @word.chars.map do |l|
+      if @guesses.include? l
+        l
+      else
+        '-'
+      end
+    end
+    matches.join
+  end
+
+  def check_win_or_lose
+    if @guesses.length == @word.length
+      :win
+    elsif @wrong_guesses.length >= 7
+      :lose
+    else
+      :play
+    end
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
